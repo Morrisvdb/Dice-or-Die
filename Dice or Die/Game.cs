fa42ad98793b34282cd441ee5d922bc6d3b78cb0
Morris van den Bergh <morrisvandenbergh@gmail.com>
2025-03-26 14:55:11 +0100

    Added dynamic buttons

--- Dice or Die/Game.cs
+++ Dice or Die/Game.cs
@@ -12,151 +12,50 @@ namespace Dice_or_Die
 {
     public partial class Game : Form
     {
+        public int rolls_left = 3;
+        public List<int> rolls;
+        private List<Button> current_buttons = new List<Button>(6);
+
         public Game()
         {
             InitializeComponent();
         }
-        public bool lock_dice1 = false;
-        public bool lock_dice2 = false;
-        public bool lock_dice3 = false;
-        public bool lock_dice4 = false;
-        public bool lock_dice5 = false;
 
-        private void die1_Click(object sender, EventArgs e)
+        private List<int> roll_dice(int count = 5)
         {
-            if (lock_dice1 == false)
+            List<int> rolls = new List<int>(count);
+            Random random = new Random();
+            for (int i = 0; i < count; i++)
             {
-                lock_dice1 = true;
-                die1.BackColor = Color.Yellow;
+                rolls.Add(random.Next(1, 7));
             }
-            else
-            {
-                lock_dice1 = false;
-                die1.BackColor = Color.White;
-            }
+
+            return rolls;
         }
 
-        private void die2_Click(object sender, EventArgs e)
+        private void draw_dice(int x, int y, List<int> rolls, int size = 50, int count = 5)
         {
-            if (lock_dice2 == false)
+            for (int i = 0; i < count; i++)
             {
-                lock_dice2 = true;
-                die2.BackColor = Color.Yellow;
+                Button b = new Button();
+                b.Name = "dice_" + i.ToString();
+                b.Text = rolls[i].ToString();
+                b.Width = size;
+                b.Height = size;
+                b.Location = new Point(x + (i * (size * 2)), y);
+                this.Controls.Add(b);
+                current_buttons.Add(b);
             }
-            else
-            {
-                lock_dice2 = false;
-                die2.BackColor = Color.White;
-            }
+            return;
         }
 
-        private void die3_Click(object sender, EventArgs e)
-        {
-            if (lock_dice3 == false)
-            {
-                lock_dice3 = true;
-                die3.BackColor = Color.Yellow;
-            }
-            else
-            {
-                lock_dice3 = false;
-                die3.BackColor = Color.White;
-            }
-        }
-
-        private void die4_Click(object sender, EventArgs e)
-        {
-            if (lock_dice4 == false)
-            {
-                lock_dice4 = true;
-                die4.BackColor = Color.Yellow;
-            }
-            else
-            {
-                lock_dice4 = false;
-                die4.BackColor = Color.White;
-            }
-        }
-
-        private void die5_Click(object sender, EventArgs e)
-        {
-            if (lock_dice5 == false)
-            {
-                lock_dice5 = true;
-                die5.BackColor = Color.Yellow;
-            }
-            else
-            {
-                lock_dice5 = false;
-                die5.BackColor = Color.White;
-            }
-        }
-
-        public int amountrolls = 3;
         private void rolldice_button_Click(object sender, EventArgs e)
         {
-            die1.Visible = true;
-            die2.Visible = true;
-            die3.Visible = true;
-            die4.Visible = true;
-            die5.Visible = true;
-            if (amountrolls != 0)
-            {
-                amountrolls -= 1;
-                amountrolls_label.Text = amountrolls.ToString();
-            }
-            else
-            {
-                amountrolls = 2;
-                amountrolls_label.Text = amountrolls.ToString();
-                lock_dice1 = false;
-                die1.BackColor = Color.White;
-                lock_dice2 = false;
-                die2.BackColor = Color.White;
-                lock_dice3 = false;
-                die3.BackColor = Color.White;
-                lock_dice4 = false;
-                die4.BackColor = Color.White;
-                lock_dice5 = false;
-                die5.BackColor = Color.White;
-            }
-            Random rng = new Random();
-            if (lock_dice1 == false)
-            {
-                int number1 = rng.Next(1, 7);
-                die1.Text = number1.ToString();
-            }
-            if (lock_dice2 == false)
-            {
-                int number2 = rng.Next(1, 7);
-                die2.Text = number2.ToString();
-            }
-            if (lock_dice3 == false)
-            {
-                int number3 = rng.Next(1, 7);
-                die3.Text = number3.ToString();
-            }
-            if (lock_dice4 == false)
-            {
-                int number4 = rng.Next(1, 7);
-                die4.Text = number4.ToString();
-            }
-            if (lock_dice5 == false)
-            {
-                int number5 = rng.Next(1, 7);
-                die5.Text = number5.ToString();
-            }
-            if (amountrolls == 0)
-            {
-                List<int> dicerow = new List<int>();
-                dicerow.Add(int.Parse(die1.Text));
-                dicerow.Add(int.Parse(die2.Text));
-                dicerow.Add(int.Parse(die3.Text));
-                dicerow.Add(int.Parse(die4.Text));
-                dicerow.Add(int.Parse(die5.Text));
-                dicerow.Sort();
-            }
-        }
+            //Button b = new Button();
 
+            List<int> rolls = roll_dice();
+
+            draw_dice(50, 50, rolls);
+        }
     }
 }
--- Dice or Die/game.Designer.cs
+++ Dice or Die/game.Designer.cs
@@ -28,125 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            die1 = new Button();
-            die2 = new Button();
-            die3 = new Button();
-            die4 = new Button();
-            die5 = new Button();
             rolldice_button = new Button();
             rollsleft_label = new Label();
             amountrolls_label = new Label();
             SuspendLayout();
             // 
-            // die1
-            // 
-            die1.Font = new Font("Segoe UI", 40F, FontStyle.Bold);
-            die1.Location = new Point(133, 159);
-            die1.Name = "die1";
-            die1.Size = new Size(75, 75);
-            die1.TabIndex = 0;
-            die1.Text = "button1";
-            die1.UseVisualStyleBackColor = true;
-            die1.Visible = false;
-            // 
-            // die2
-            // 
-            die2.Font = new Font("Segoe UI", 40F, FontStyle.Bold);
-            die2.Location = new Point(214, 159);
-            die2.Name = "die2";
-            die2.Size = new Size(75, 75);
-            die2.TabIndex = 1;
-            die2.Text = "button2";
-            die2.UseVisualStyleBackColor = true;
-            die2.Visible = false;
-            // 
-            // die3
-            // 
-            die3.Font = new Font("Segoe UI", 40F, FontStyle.Bold);
-            die3.Location = new Point(295, 159);
-            die3.Name = "die3";
-            die3.Size = new Size(75, 75);
-            die3.TabIndex = 2;
-            die3.Text = "button3";
-            die3.UseVisualStyleBackColor = true;
-            die3.Visible = false;
-            // 
-            // die4
-            // 
-            die4.Font = new Font("Segoe UI", 40F, FontStyle.Bold);
-            die4.Location = new Point(376, 159);
-            die4.Name = "die4";
-            die4.Size = new Size(75, 75);
-            die4.TabIndex = 3;
-            die4.Text = "button4";
-            die4.UseVisualStyleBackColor = true;
-            die4.Visible = false;
-            // 
-            // die5
-            // 
-            die5.Font = new Font("Segoe UI", 40F, FontStyle.Bold);
-            die5.Location = new Point(457, 159);
-            die5.Name = "die5";
-            die5.Size = new Size(75, 75);
-            die5.TabIndex = 4;
-            die5.Text = "button5";
-            die5.UseVisualStyleBackColor = true;
-            die5.Visible = false;
-            // 
             // rolldice_button
             // 
             rolldice_button.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
-            rolldice_button.Location = new Point(295, 253);
+            rolldice_button.Location = new Point(337, 337);
+            rolldice_button.Margin = new Padding(3, 4, 3, 4);
             rolldice_button.Name = "rolldice_button";
-            rolldice_button.Size = new Size(75, 33);
+            rolldice_button.Size = new Size(86, 44);
             rolldice_button.TabIndex = 5;
             rolldice_button.Text = "Roll";
             rolldice_button.UseVisualStyleBackColor = true;
+            rolldice_button.Click += rolldice_button_Click;
             // 
             // rollsleft_label
             // 
             rollsleft_label.AutoSize = true;
-            rollsleft_label.Location = new Point(381, 264);
+            rollsleft_label.Location = new Point(435, 352);
             rollsleft_label.Name = "rollsleft_label";
-            rollsleft_label.Size = new Size(52, 15);
+            rollsleft_label.Size = new Size(66, 20);
             rollsleft_label.TabIndex = 6;
             rollsleft_label.Text = "rolls left:";
             // 
             // amountrolls_label
             // 
             amountrolls_label.AutoSize = true;
-            amountrolls_label.Location = new Point(439, 264);
+            amountrolls_label.Location = new Point(502, 352);
             amountrolls_label.Name = "amountrolls_label";
-            amountrolls_label.Size = new Size(13, 15);
+            amountrolls_label.Size = new Size(17, 20);
             amountrolls_label.TabIndex = 7;
             amountrolls_label.Text = "3";
             // 
-            // game
+            // Game
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 293);
+            ClientSize = new Size(914, 391);
             Controls.Add(amountrolls_label);
             Controls.Add(rollsleft_label);
             Controls.Add(rolldice_button);
-            Controls.Add(die5);
-            Controls.Add(die4);
-            Controls.Add(die3);
-            Controls.Add(die2);
-            Controls.Add(die1);
-            Name = "game";
+            Margin = new Padding(3, 4, 3, 4);
+            Name = "Game";
             Text = "game";
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
-
-        private Button die1;
-        private Button die2;
-        private Button die3;
-        private Button die4;
-        private Button die5;
         private Button rolldice_button;
         private Label rollsleft_label;
         private Label amountrolls_label;
