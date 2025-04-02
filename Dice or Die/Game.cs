d5be988f7daae602aaf6bfa40ceaace725966614
Morris van den Bergh <morrisvandenbergh@gmail.com>
2025-04-02 14:55:33 +0200

    Added random Upgrades

--- Dice or Die/Game.cs
+++ Dice or Die/Game.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
+using System.Web;
 using System.Windows.Forms;
 using System.Xml.Serialization;
 
@@ -18,15 +19,50 @@ namespace Dice_or_Die
         public Dictionary<string, KeyValuePair<int, bool>> dice = new Dictionary<string, KeyValuePair<int, bool>>(); // { button_name(string): { value{int}: locked{bool} } }
         private List<Button> current_buttons = new List<Button>();
         private int dice_count = 5;
+        private int roll_count = 3;
         public int current_player = 1;
         public int current_round = 1;
         public int current_health = 100;
         public int current_money = 0;
         public int pending_damage = 0;
+        public int outgoing_damage = 0;
+        public int max_health = 10;
+        public bool in_shop = false;
+        public int shield = 0;
+        public int grace = 0;
+        public int damage_multiplier = 1;
 
         public Game()
         {
             InitializeComponent();
+            this.FormClosed += save_on_close;
+        }
+
+        public class PlayerData
+        {
+            public int player_id { get; set; }
+            public int health { get; set; }
+            public int money { get; set; }
+            public int dice_count { get; set; }
+            public int roll_count { get; set; }
+            public int outgoing_damage { get; set; }
+            public int max_health { get; set; }
+            public int shield { get; set; }
+            public int grace { get; set; } // How many rounds of 100% damage reduction left
+            public int damage_multiplier { get; set; }
+        }
+        public class Upgrade
+        {
+            public required string name { get; set; }
+            public required int cost { get; set; }
+            public required string description { get; set; }
+            public required string type { get; set; }
+            public required int value { get; set; }
+        }
+
+        private void save_on_close(object FormClosedEventHandeler, EventArgs e)
+        {
+            save_game();
         }
 
         private void init_dice(int count = 5)
@@ -76,7 +112,7 @@ namespace Dice_or_Die
                     b.BackColor = Color.LightGreen;
                 }
                 current_buttons.Add(b);
-                this.Controls.Add(b);
+                gamePanel.Controls.Add(b);
                 x += size + 10;
             }
 
@@ -116,33 +152,148 @@ namespace Dice_or_Die
 
         private void rolldice_button_Click(object sender, EventArgs e)
         {
-            switch_players();
-            //if (rolls_left <= 0)
-            //{
-            //    return;
-            //}
-            //rolls_left--;
-            //amountrolls_label.Text = rolls_left.ToString();
-            //if (rolls_left == 0)
-            //{
-            //    rolldice_button.Enabled = false;
-            //}
-            //roll_dice(dice_count);
+            if (rolls_left <= 0)
+            {
+                start_shop();
+                return;
+            }
+            rolls_left--;
+            amountrolls_label.Text = rolls_left.ToString();
+            if (rolls_left == 0)
+            {
+                rolldice_button.Text = "Shop";
+            }
+            roll_dice(dice_count);
 
-            //draw_dice(x: 50, y: 50, count: dice_count);
+            draw_dice(x: 100, y: 100, count: dice_count);
+        }
+
+        private void start_shop()
+        {
+            in_shop = true;
+            gamePanel.Visible = false;
+            shopPanel.Visible = true;
+            moneyLabelShop.Text = "Money: " + current_money.ToString();
+            populate_shop();
+        }
+
+        private void populate_shop()
+        {
+            string path = @"C:\Informatica\Dice or Die\Dice or Die\Data\upgrades.json";
+            string json_in = File.ReadAllText(path);
+            List<Upgrade> _data = JsonSerializer.Deserialize<List<Upgrade>>(json_in);
+
+            Random rand = new Random();
+            List<Upgrade> selected_upgrades = new List<Upgrade>();
+            for (int i = 0; i < 3; i++)
+            {
+                int r = rand.Next(0, _data.Count);
+                selected_upgrades.Add(_data[r]);
+                _data.RemoveAt(r);
+            }
+            upgradesBox.Items.Clear();
+            foreach (var upgrade in selected_upgrades)
+            {
+                upgradesBox.Items.Add(new Upgrade { name = upgrade.name, cost = upgrade.cost, description = upgrade.description, type = upgrade.type, value = upgrade.value });
+            }
+        }
+
+        private void resolve_upgrade(Upgrade upgrade)
+        {
+            switch (upgrade.type)
+            {
+                case "heal":
+                    if (current_health + upgrade.value <= max_health)
+                    {
+                        current_health += upgrade.value;
+                    }
+                    else
+                    {
+                        current_health = max_health;
+                    }
+                    break;
+                case "damage":
+                    int value;
+                    if (int.TryParse(upgrade.value.ToString(), out value))
+                    {
+                        outgoing_damage += value;
+                        break;
+                    }
+                    if (upgrade.value.ToString().Split().Contains("x"))
+                    {
+                        int multiplier = int.Parse(upgrade.value.ToString().Split('x')[0]);
+                        damage_multiplier *= multiplier;
+                        break;
+                    }
+
+
+                    break;
+            }
+        }
+
+        private void end_shop()
+        {
+            in_shop = false;
+            gamePanel.Visible = true;
+            shopPanel.Visible = false;
+            switch_players();
         }
 
         private void Game_Load(object sender, EventArgs e)
         {
             init_dice(dice_count);
+            load_game();
         }
 
         private void switch_players()
         {
+            save_game();
+
+            current_player = current_player == 1 ? 2 : 1;
+
+            load_game();
+        }
+
+        private void load_game()
+        {
+            string other_player = current_player == 1 ? "2" : "1";
+            string json_ = File.ReadAllText(@"C:\Informatica\Dice or Die\player_" + other_player + "_data.json");
+            List<PlayerData> _data = JsonSerializer.Deserialize<List<PlayerData>>(json_);
+            pending_damage = _data[0].outgoing_damage;
+
+
+            string path = @"C:\Informatica\Dice or Die\player_" + current_player + "_data.json";
+            string json_in = File.ReadAllText(path);
+            List<PlayerData> _data_in = JsonSerializer.Deserialize<List<PlayerData>>(json_in);
+            current_health = _data_in[0].health;
+            current_money = _data_in[0].money;
+            dice_count = _data_in[0].dice_count;
+            roll_count = _data_in[0].roll_count;
+            shield = _data_in[0].shield;
+            grace = _data_in[0].grace;
+            damage_multiplier = _data_in[0].damage_multiplier;
+
+            if (_data_in[0].max_health != 0)
+            {
+                max_health = _data_in[0].max_health;
+            }
+            else
+            {
+                max_health = 10;
+            }
+
+            rolls_left = roll_count;
+            amountrolls_label.Text = rolls_left.ToString();
+            healthLabel.Text = "Health: " + current_health.ToString();
+            moneyLabel.Text = "Money: " + current_money.ToString();
+
+        }
+
+        private void save_game()
+        {
             string path = @"C:\Informatica\Dice or Die\player_" + current_player + "_data.json";
             File.AppendAllText(path, "");
             List<PlayerData> _data = new List<PlayerData>();
-
 
             _data.Add(new PlayerData
             {
@@ -150,30 +301,42 @@ namespace Dice_or_Die
                 health = current_health,
                 money = current_money,
                 dice_count = dice_count,
-                pending_damage = pending_damage
+                roll_count = roll_count,
+                damage_multiplier = damage_multiplier,
+                max_health = max_health,
+                shield = shield,
+                grace = grace,
+                outgoing_damage = outgoing_damage,
             });
 
             string json_out = JsonSerializer.Serialize(_data);
             File.WriteAllText(path, json_out);
 
-            current_player = current_player == 1 ? 2 : 1;
-
-            string json_in = File.ReadAllText(path);
-            List<PlayerData> _data_in = JsonSerializer.Deserialize<List<PlayerData>>(json_in);
-
-            current_health = _data_in[0].health;
-            current_money = _data_in[0].money;
-            dice_count = _data_in[0].dice_count;
-            pending_damage = _data_in[0].pending_damage;
         }
 
-        public class PlayerData
+        private void continueFromShopButton_Click(object sender, EventArgs e)
         {
-            public int player_id { get; set; }
-            public int health { get; set; }
-            public int money { get; set; }
-            public int dice_count { get; set; }
-            public int pending_damage { get; set; }
+            end_shop();
+        }
+
+        private Upgrade fetch_upgrade(string name = "none")
+        {
+            string path = @"C:\Informatica\Dice or Die\Dice or Die\Data\upgrades.json";
+            string json_in = File.ReadAllText(path);
+            List<Upgrade> _data = JsonSerializer.Deserialize<List<Upgrade>>(json_in);
+            foreach (var upgrade in _data)
+            {
+                if (upgrade.name == name)
+                {
+                    return upgrade;
+                }
+            }
+            return new Upgrade{ name = "Help", cost = 20, description = "Help, something went terribly wrong", type = "heal", value = 1 };
+        }
+
+        private void buyButton_Click(object sender, EventArgs e)
+        {
+            resolve_upgrade(fetch_upgrade(upgradesBox.SelectedValue.ToString()));
         }
     }
 }
--- Dice or Die/game.Designer.cs
+++ Dice or Die/game.Designer.cs
@@ -28,70 +28,173 @@
         /// </summary>
         private void InitializeComponent()
         {
-            rolldice_button = new Button();
-            rollsleft_label = new Label();
+            continueFromShopButton = new Button();
             amountrolls_label = new Label();
             label1 = new Label();
+            rollsleft_label = new Label();
+            rolldice_button = new Button();
+            gamePanel = new Panel();
+            moneyLabel = new Label();
+            healthLabel = new Label();
+            shopPanel = new Panel();
+            buyButton = new Button();
+            moneyLabelShop = new Label();
+            upgradesBox = new ListBox();
+            gamePanel.SuspendLayout();
+            shopPanel.SuspendLayout();
             SuspendLayout();
             // 
-            // rolldice_button
+            // continueFromShopButton
             // 
-            rolldice_button.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
-            rolldice_button.Location = new Point(295, 253);
-            rolldice_button.Name = "rolldice_button";
-            rolldice_button.Size = new Size(75, 33);
-            rolldice_button.TabIndex = 5;
-            rolldice_button.Text = "Roll";
-            rolldice_button.UseVisualStyleBackColor = true;
-            rolldice_button.Click += rolldice_button_Click;
-            // 
-            // rollsleft_label
-            // 
-            rollsleft_label.AutoSize = true;
-            rollsleft_label.Location = new Point(381, 264);
-            rollsleft_label.Name = "rollsleft_label";
-            rollsleft_label.Size = new Size(52, 15);
-            rollsleft_label.TabIndex = 6;
-            rollsleft_label.Text = "rolls left:";
+            continueFromShopButton.Location = new Point(174, 231);
+            continueFromShopButton.Name = "continueFromShopButton";
+            continueFromShopButton.Size = new Size(94, 29);
+            continueFromShopButton.TabIndex = 0;
+            continueFromShopButton.Text = "Continue";
+            continueFromShopButton.UseVisualStyleBackColor = true;
+            continueFromShopButton.Click += continueFromShopButton_Click;
             // 
             // amountrolls_label
             // 
             amountrolls_label.AutoSize = true;
-            amountrolls_label.Location = new Point(439, 264);
+            amountrolls_label.Location = new Point(362, 225);
             amountrolls_label.Name = "amountrolls_label";
-            amountrolls_label.Size = new Size(13, 15);
+            amountrolls_label.Size = new Size(17, 20);
             amountrolls_label.TabIndex = 7;
             amountrolls_label.Text = "3";
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(25, 264);
+            label1.Location = new Point(105, 231);
             label1.Name = "label1";
-            label1.Size = new Size(38, 15);
+            label1.Size = new Size(50, 20);
             label1.TabIndex = 8;
             label1.Text = "label1";
             // 
+            // rollsleft_label
+            // 
+            rollsleft_label.AutoSize = true;
+            rollsleft_label.Location = new Point(290, 225);
+            rollsleft_label.Name = "rollsleft_label";
+            rollsleft_label.Size = new Size(66, 20);
+            rollsleft_label.TabIndex = 6;
+            rollsleft_label.Text = "rolls left:";
+            // 
+            // rolldice_button
+            // 
+            rolldice_button.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            rolldice_button.Location = new Point(198, 216);
+            rolldice_button.Margin = new Padding(3, 4, 3, 4);
+            rolldice_button.Name = "rolldice_button";
+            rolldice_button.Size = new Size(86, 44);
+            rolldice_button.TabIndex = 5;
+            rolldice_button.Text = "Roll";
+            rolldice_button.UseVisualStyleBackColor = true;
+            rolldice_button.Click += rolldice_button_Click;
+            // 
+            // gamePanel
+            // 
+            gamePanel.Controls.Add(moneyLabel);
+            gamePanel.Controls.Add(healthLabel);
+            gamePanel.Controls.Add(rolldice_button);
+            gamePanel.Controls.Add(amountrolls_label);
+            gamePanel.Controls.Add(rollsleft_label);
+            gamePanel.Controls.Add(label1);
+            gamePanel.Location = new Point(12, 12);
+            gamePanel.Name = "gamePanel";
+            gamePanel.Size = new Size(510, 281);
+            gamePanel.TabIndex = 9;
+            // 
+            // moneyLabel
+            // 
+            moneyLabel.AutoSize = true;
+            moneyLabel.Location = new Point(50, 68);
+            moneyLabel.Name = "moneyLabel";
+            moneyLabel.Size = new Size(57, 20);
+            moneyLabel.TabIndex = 10;
+            moneyLabel.Text = "Money:";
+            // 
+            // healthLabel
+            // 
+            healthLabel.AutoSize = true;
+            healthLabel.Location = new Point(47, 48);
+            healthLabel.Name = "healthLabel";
+            healthLabel.Size = new Size(60, 20);
+            healthLabel.TabIndex = 9;
+            healthLabel.Text = "Health: ";
+            // 
+            // shopPanel
+            // 
+            shopPanel.Controls.Add(buyButton);
+            shopPanel.Controls.Add(moneyLabelShop);
+            shopPanel.Controls.Add(upgradesBox);
+            shopPanel.Controls.Add(continueFromShopButton);
+            shopPanel.Location = new Point(528, 12);
+            shopPanel.Name = "shopPanel";
+            shopPanel.Size = new Size(452, 281);
+            shopPanel.TabIndex = 10;
+            shopPanel.Visible = false;
+            // 
+            // buyButton
+            // 
+            buyButton.Location = new Point(174, 178);
+            buyButton.Name = "buyButton";
+            buyButton.Size = new Size(94, 29);
+            buyButton.TabIndex = 12;
+            buyButton.Text = "Buy";
+            buyButton.UseVisualStyleBackColor = true;
+            buyButton.Click += buyButton_Click;
+            // 
+            // moneyLabelShop
+            // 
+            moneyLabelShop.AutoSize = true;
+            moneyLabelShop.Location = new Point(15, 15);
+            moneyLabelShop.Name = "moneyLabelShop";
+            moneyLabelShop.Size = new Size(57, 20);
+            moneyLabelShop.TabIndex = 11;
+            moneyLabelShop.Text = "Money:";
+            // 
+            // upgradesBox
+            // 
+            upgradesBox.DisplayMember = "description";
+            upgradesBox.FormattingEnabled = true;
+            upgradesBox.Location = new Point(35, 68);
+            upgradesBox.Name = "upgradesBox";
+            upgradesBox.Size = new Size(378, 104);
+            upgradesBox.TabIndex = 1;
+            upgradesBox.ValueMember = "name";
+            // 
             // Game
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 293);
-            Controls.Add(label1);
-            Controls.Add(amountrolls_label);
-            Controls.Add(rollsleft_label);
-            Controls.Add(rolldice_button);
+            ClientSize = new Size(1435, 391);
+            Controls.Add(shopPanel);
+            Controls.Add(gamePanel);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "Game";
             Text = "game";
             Load += Game_Load;
+            gamePanel.ResumeLayout(false);
+            gamePanel.PerformLayout();
+            shopPanel.ResumeLayout(false);
+            shopPanel.PerformLayout();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
-        private Button rolldice_button;
-        private Label rollsleft_label;
+        private Button continueFromShopButton;
         private Label amountrolls_label;
         private Label label1;
+        private Label rollsleft_label;
+        private Button rolldice_button;
+        private Panel gamePanel;
+        private Panel shopPanel;
+        private Label moneyLabel;
+        private Label healthLabel;
+        private Label moneyLabelShop;
+        private ListBox upgradesBox;
+        private Button buyButton;
     }
 }
